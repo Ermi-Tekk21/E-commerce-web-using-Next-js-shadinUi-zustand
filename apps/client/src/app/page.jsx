b318a1d586cc7b805c76457a63b669c4d07a42fb
Ermias-TekileMarkos <ermaatekk@gmail.com>
2024-05-13 15:30:03 +0300

    intial commit

---
+++ apps/client/src/app/page.jsx
@@ -0,0 +1,12 @@
+import LandingPage from "../components/landingPage/landingPage";
+
+export default function Page() {
+  return (
+    <main className="w-full container">
+      <div className="w-full py-2">
+      <LandingPage/>
+        
+      </div>
+    </main>
+  );
+}
